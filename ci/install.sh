8c70c45913b6481569a9b511f4de481f2d2d756f
Zane Hitchcox <zane@balena.io>
2021-03-04 17:30:15 -0500

    openssl

--- ci/install.sh
+++ ci/install.sh
@@ -4,6 +4,7 @@ main() {
     local target=
     if [ $OS_NAME = linux ]; then
         sudo apt-get install libssl-dev
+        sudo apt-get install openssl
         target=x86_64-unknown-linux-musl
         sort=sort
     else
