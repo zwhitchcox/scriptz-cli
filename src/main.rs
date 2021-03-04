b1c1443579107f05d84b71f1361569473ecd7ca2
Zane Hitchcox <zane@balena.io>
2021-03-04 17:14:36 -0500

    fix https

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 extern crate rpassword;
-use hyper::{Client, Request, StatusCode, Body, body};
+use hyper::StatusCode;
 use structopt::StructOpt;
 use std::thread;
 use webbrowser::{self, BrowserOptions};
@@ -9,7 +9,6 @@ use std::fs;
 use std::fs::File;
 use std::io::{self, Write};
 use std::process::{Command};
-use serde_json::{Value, json};
 
 
 #[derive(StructOpt, Debug)]
@@ -35,12 +34,6 @@ enum Cmd {
   },
   Logout {},
   Ls {},
-  AddKey {
-    #[structopt(required = true)]
-    name: String,
-    #[structopt(required = true)]
-    file: String,
-  }
 }
 
 fn is_sh(str: String) -> bool {
@@ -60,31 +53,33 @@ fn get_origin() -> String {
   return String::from("https://scriptz.sh")
 }
 
+async fn send_get(endpoint: String, token: Option<String>) -> String {
+  let client = reqwest::Client::new();
+  let url = format!("{}{}", get_origin(), endpoint);
+  // panic!();
+  let mut req = client
+    .get(&url);
+  if token.is_some() {
+    req = req
+    .header("Authorization", format!("Bearer {}", token.unwrap()))
+  }
+  let res = req
+    .send()
+    .await.unwrap();
+
+  let status = res.status();
+  let text = res.text().await.unwrap();
+  if status != StatusCode::OK {
+    panic!(text)
+  }
+  text
+}
+
 fn get_token() -> String {
   fs::read_to_string(get_token_file()).unwrap_or(String::from(""))
 }
-async fn get_file(filename: String, token: String) ->
-  Result<String, reqwest::Error> {
-  let client = reqwest::Client::new();
-  let url = get_origin() + "/d/" + &filename.as_str();
-  let res = client
-    .get(&url)
-    .header("Authorization", format!("Bearer {}", token))
-    .send()
-    .await?;
-
-  let status = res.status();
-  let text = res.text().await?;
-  if status == StatusCode::UNAUTHORIZED {
-    panic!(text)
-  }
-  if status == StatusCode::NOT_FOUND {
-    panic!(text)
-  }
-  if status != StatusCode::OK {
-    panic!("{}", text)
-  }
-  Ok(text)
+async fn get_file(filename: String) -> String {
+  send_get(format!("/d/{}", filename), Some(get_token())).await
 }
 
 async fn make_temp_file(filename: String, file_contents: String)
@@ -100,8 +95,8 @@ async fn make_temp_file(filename: String, file_contents: String)
 }
 
 
-async fn run_script(filename: String) -> Result<(), reqwest::Error> {
-  let file_contents = get_file(filename.clone(), get_token()).await?;
+async fn run_script(filename: String) {
+  let file_contents = get_file(filename.clone()).await;
   let temp_file = make_temp_file(filename.clone(), file_contents.clone()).await
     .expect("Couldn't make temporary file.");
   let child = Command::new("bash")
@@ -110,41 +105,16 @@ async fn run_script(filename: String) -> Result<(), reqwest::Error> {
     .expect("The script failed.");
   child.wait_with_output()
     .expect("There was an error running the file.");
-  Ok(())
 }
 
 async fn list() {
-  let client = Client::new();
-  let req = Request::builder()
-    .method("GET")
-    .uri(get_origin() + "/api/cli/script-names")
-    .header("Authorization", format!("Bearer {}", get_token()))
-    .body(Body::from(""))
-    .unwrap();
-
-
-  match client.request(req).await {
-    Ok(res) => {
-      if res.status() == StatusCode::UNAUTHORIZED {
-        panic!("You need to log in. (`scriptz login`).")
-      }
-      if res.status() != StatusCode::OK {
-        panic!("{}", res.status())
-      }
-      let body_bytes = body::to_bytes(res.into_body()).await.unwrap();
-      let list = String::from_utf8(body_bytes.to_vec()).unwrap();
-      println!("{}", list);
-    },
-    Err(err) => {
-      println!("Error: {}", err);
-      panic!("Process exiting...")
-    },
-  }
+  let ls = send_get("/api/cli/script-names".to_string(), Some(get_token())).await;
+  println!("{}", ls)
 }
 
 async fn show_script(file: String) {
-  let file = get_file(file, get_token()).await;
-  println!("{}", file.unwrap());
+  let file = get_file(file).await;
+  println!("{}", file);
 }
 
 fn login(mut token: String) {
@@ -172,44 +142,6 @@ fn login(mut token: String) {
     .expect("Couldn't create .scriptz dir");
   std::fs::write(scriptz_dir.join("token").clone(), token)
     .expect("Couldn't write token to scriptz dir");
-}
-
-async fn send_post(endpoint: String, value: Value) {
-  let req = Request::post(get_origin() + endpoint.as_str())
-    .header("Authorization", format!("Bearer {}", get_token()))
-    .header("Content-Type", "application/json")
-    .body(Body::from(serde_json::to_string(&value).unwrap()))
-    .unwrap();
-
-  let client = Client::new();
-  match client.request(req).await {
-    Ok(res) => {
-      let status = res.status();
-      let body_bytes = body::to_bytes(res.into_body()).await.unwrap();
-      let response = String::from_utf8(body_bytes.to_vec()).unwrap();
-      println!("{}", response);
-      if status == StatusCode::UNAUTHORIZED {
-        panic!("You need to log in. (`scriptz login`).")
-      }
-      if status != StatusCode::OK {
-        panic!("{}", status)
-      }
-    },
-    Err(err) => {
-      println!("Error: {}", err);
-      panic!("Process exiting...")
-    },
-  }
-}
-
-async fn add_key(name: String, file: String) {
-  println!("Adding key: {}, {}", name, file);
-  let contents = fs::read_to_string(file)
-    .expect("Something went wrong reading the key file");
-  send_post(String::from("/api/gh/add-ssh-key"), json!({
-    "key": contents,
-    "name": name,
-  })).await;
 }
 
 #[tokio::main]
@@ -247,9 +179,5 @@ async fn main() -> Result<(), ()> {
       list().await;
       Ok(())
     },
-    Cmd::AddKey{ name, file } => {
-      add_key(name, file).await;
-      Ok(())
-    }
   }
 }
